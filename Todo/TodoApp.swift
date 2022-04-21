de3bff81159b073af25bfff4744d159987ed8a72
apriakhin <apriakhin@aol.com>
2022-04-22 00:12:48 +0300

    Create project

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Anton Priakhin on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
